b3fd407ec80d08463f0eac117200fedc266088a2
SrikanthReddyMallu <126140093+SrikanthReddyMallu@users.noreply.github.com>
2023-03-09 23:19:48 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,44 @@
+package com.students;
+
+public class College 
+{
+private int id;
+private String name;
+private String website;
+private Branch branch;
+private Student student;
+
+
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getWebsite() {
+	return website;
+}
+public void setWebsite(String website) {
+	this.website = website;
+}
+public Branch getBranch() {
+	return branch;
+}
+public void setBranch(Branch branch) {
+	this.branch = branch;
+}
+public Student getStudent() {
+	return student;
+}
+public void setStudent(Student student) {
+	this.student = student;
+}
+
+
+}
